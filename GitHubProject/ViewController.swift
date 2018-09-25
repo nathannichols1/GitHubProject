a387acfb730e623382c4cc4038c2087f23f3bc22
NATHAN NICHOLS <rl235817@rhs-macair-40.rlas-116.org>
2018-09-25 10:32:47 -0500

    Initial Commit

---
+++ GitHubProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubProject
+//
+//  Created by NATHAN NICHOLS on 9/25/18.
+//  Copyright © 2018 NATHAN NICHOLS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
